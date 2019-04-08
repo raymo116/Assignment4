b4ca12d82b5d527a993bd25638033568cfd4843f
Christian Valencia <31971056+SaintOcean@users.noreply.github.com>
2019-04-08 13:05:55 -0700

    Added regex error checking for input

--- fileIO.cpp
+++ fileIO.cpp
@@ -1,58 +1,58 @@
 #include <iostream>
 #include <fstream>
-// ToDo: remove cstring #include <cstring>
+#include <cstring>
 #include <string>
 #include <stdlib.h>
+#include <regex>
 
 #include "genQ.h"
 #include "fileIO.h"
 
 using namespace std;
 
-// Deafault constructor
 FileIO::FileIO()
 {
     filepath = "file.test";
 }
-
-// Overloaded constructor
 FileIO::FileIO(string fp)
 {
     filepath = fp;
-
-    // Checks to make sure the file exists
-    ifstream file (filepath);
-    if(!file)
-        throw invalid_argument( "received invalid filepath: " + filepath);
-    else
-        file.close();
 }
-
-// Destructor
 FileIO::~FileIO()
 {
     delete output;
 }
 
-// Fills a Queue with the data from the file and returns it
 GenQ<int>* FileIO::getQueue()
 {
     ifstream file (filepath);
     output = new GenQ<int>();
 
-    file.clear();
-    file.seekg(0);
-
-    string line;
-    while (getline (file, line))
+    if(file.is_open())
     {
-        if(line != "")
-        {
-            output->insert(atoi(line.c_str()));
-        }
+        file.clear();
+        file.seekg(0);
 
+        string line;
+        while (getline (file, line))
+        {
+            if(line != "" && regex_match(line,regex("[0-9]+")))
+            {
+                output->insert(atoi(line.c_str()));
+            }
+            else
+            {
+                cout << "Invalid input in file.\n" << endl;
+                break;
+            }
+
+        }
+        file.close();
     }
-    file.close();
+    else
+    {
+        cout << "Invalid filepath" << endl;
+    }
 
     return output;
 }
